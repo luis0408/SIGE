aa4a6250e246b8bdcf639dcab81350e7893683db
Usuario <Usuario@DESKTOP-0FTL6AM>
2024-03-11 22:15:38 -0600

    subi cambio

---
+++ SIGE/SIGE/SIGE/FORMULARIOS/Login.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SIGE.SIGE.FORMULARIOS
+{
+    
+
+    public partial class Login : System.Web.UI.Page
+
+    {
+        utilerias objeto = new utilerias();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            string correo = "";
+
+            object[] dat = {correo };
+            string[] par = {"@correo" };
+            DataSet resultado = objeto.consultarProcedimiento("inicioSesion",dat,par,);
+
+            
+        }
+    }
+}
---
+++ SIGE/SIGE/utilerias.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SIGE
+{
+    public class utilerias
+    {
+        public DataSet consultarProcedimiento(string procedimiento, object[] datos, string[] parametros)
+        {
+            DataSet ds = null;
+            return ds;
+            DataTable dt = ds.Tables[0];
+
+        }
+
+    }
+}
